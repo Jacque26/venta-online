e719f13e9d7ea59c824a9e5aac7c4f2a0f8b4f82
Jacque26 <jake26794@gmail.com>
2023-03-03 08:49:30 +0000

    construccion de pag Categorias

---
+++ src/components/category-Item/category-item.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./category-item.css"
+
+const CategoryItem = ({ title, img }) => {
+    return (
+        <div className='category-container'>
+            <div className="background-image" style={{
+                backgroundImage: `url(${img})`
+            }}></div>z
+            <div className='category-body-container'>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    );
+};
+
+export default CategoryItem;
---
+++ src/components/category-List/Category-List.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import CategoryItem from '../category-Item/category-item';
+
+const CategoryList= ({categories})=>{
+    return(
+        <div className='categories-container'>
+        {categories.map((category) => {
+          return (
+            <CategoryItem 
+            title={category.title} 
+            key={category.id} 
+            img={category.img} />)
+        })}
+  
+      </div>
+    );
+};
+export default CategoryList;
